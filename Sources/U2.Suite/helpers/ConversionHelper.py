a878657bf3637b86553ddb6144db1ffb0215e9e4
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-08-07 00:55:53 +0300

    Added new methods and unit tests

--- Sources/U2.Suite/helpers/ConversionHelper.py
+++ Sources/U2.Suite/helpers/ConversionHelper.py
@@ -16,8 +16,30 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import re
+from contracts.BitMask import BitMask
+from contracts.RigParameter import RigParameter
+from exceptions.MaskParseException import MaskParseException
+from exceptions.ParameterParseException import ParameterParseException
+from typing import List
 
 class ConversionHelper():
+    
+    @staticmethod
+    def BytesAnd(array1 : bytearray, array2 : bytearray) -> bytearray:
+        result = bytearray()
+
+        max_index = min(len(array1), len(array2))
+        array1_1 = array1[:max_index]
+        array2_1 = array2[:max_index]
+        
+        index = 0
+        for x in array1_1:
+            value = x & array2_1[index]
+            result.append(value)
+            index = index + 1
+
+        return result
+        
     
     @staticmethod
     def HexStrToBytes(s : str) -> bytearray:
@@ -39,3 +61,73 @@ class ConversionHelper():
         
         elif ('0123456789abcdef'.find(trimmed[0].lower()) > -1):
             return ConversionHelper.HexStrToBytes(trimmed)
+    
+    @staticmethod
+    def SplitString(s : str) -> List[str]:
+        return s.split('|')
+    
+    @staticmethod
+    def FlagsFromBitMask(mask: bytearray, is_string: bool) -> bytearray:
+        flags = mask
+        dot = ord('.')
+        
+        index = 0
+        if (is_string):
+            for m in mask:
+                if (m == dot):
+                    mask[index] = 0x00
+                    flags[index] = 0x00
+                else:
+                    mask[index] = 0xFF
+                index = index + 1
+        else:
+            for m in mask:
+                if (m != 0x00):
+                    mask[index] = 0xFF
+                index = index + 1             
+    
+    @staticmethod
+    def StrToRigParameter(s : str) -> RigParameter:
+        if not s.startswith('pm'):
+            raise ParameterParseException('Cannot recognize the parameter {}'.format(s))
+        
+        statuses = [status for status in dir(
+            RigParameter) if not status.startswith('_')]
+        
+        cut_value = s.strip().removeprefix('pm').lower()
+        if cut_value in statuses:
+            return eval('RigParameter.' + cut_value)
+        
+        raise ParameterParseException('Cannot parse parameter {}'.format(s))
+        
+    @staticmethod
+    def StrToBitMask(s : str) -> BitMask:
+        input_string = s.strip()
+        result = BitMask()
+        
+        if len(input_string) == 0:
+            return result
+        
+        list = ConversionHelper.SplitString(input_string)
+        result.Mask = ConversionHelper.StrToBytes(list[0])
+        
+        is_string = list[0][0] == '('
+        size = len(list)
+        if size == 1:
+            is_string = list[0][0] == '('
+            result.Flags = ConversionHelper.FlagsFromBitMask(result.Mask, is_string)
+        
+        elif size == 2:
+            result.Param = ConversionHelper.StrToRigParameter(list[1])
+
+            if result.Param != RigParameter.none:
+                result.Flags = ConversionHelper.FlagsFromBitMask(result.Mask, is_string)
+            else:
+                result.Flags = result.Mask
+        elif size == 3:
+            result.Flags = ConversionHelper.StrToBytes(list[1])
+            result.Param = ConversionHelper.StrToRigParameter(list[2])
+        else:
+            raise MaskParseException('Cannot parse mask {}'.format(input_string))
+
+        return result
--- Sources/U2.Suite/tests/test_ConversionHelper.py
+++ Sources/U2.Suite/tests/test_ConversionHelper.py
@@ -15,27 +15,51 @@
 # You should have received a copy of the GNU General Public License 
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from pyrsistent import b
+from exceptions.ParameterParseException import ParameterParseException
+import unittest
 from helpers.ConversionHelper import ConversionHelper as ch
+from contracts.RigParameter import RigParameter as rp
 
-def test_hex_str_to_bytes():
-    expected = bytearray(b'ABC')
-    actual = ch.HexStrToBytes('414243')
-    assert expected == actual
-    actual = ch.HexStrToBytes('41.42.43')
-    assert expected == actual
-    actual = ch.HexStrToBytes('41 42 43')
-    assert expected == actual
-    
-    expected = []
-    actual = ch.HexStrToBytes('411') # odd number of characters
-    assert expected == actual
-    actual = ch.HexStrToBytes('41.1') # odd number with a dot
-    assert expected == actual
-    actual = ch.HexStrToBytes('41 1') # odd number with space
-    assert expected == actual
-    
-def test_str_to_bytes():
-    expected = bytearray(b'AB\x00CY')
-    actual = ch.StrToBytes('(AB.CY)')
-    assert expected == actual
-    
+class RigParameterTests(unittest.TestCase):
+    def test_hex_str_to_bytes():
+        expected = bytearray(b'ABC')
+        actual = ch.HexStrToBytes('414243')
+        assert expected == actual
+        actual = ch.HexStrToBytes('41.42.43')
+        assert expected == actual
+        actual = ch.HexStrToBytes('41 42 43')
+        assert expected == actual
+        
+        expected = []
+        actual = ch.HexStrToBytes('411') # odd number of characters
+        assert expected == actual
+        actual = ch.HexStrToBytes('41.1') # odd number with a dot
+        assert expected == actual
+        actual = ch.HexStrToBytes('41 1') # odd number with space
+        assert expected == actual
+        
+    def test_str_to_bytes():
+        expected = bytearray(b'AB\x00CY')
+        actual = ch.StrToBytes('(AB.CY)')
+        assert expected == actual
+
+    def test_parse_rig_parameter(self):
+        assert rp.none == ch.StrToRigParameter('pmNone')
+        
+        # non existent value
+        with self.assertRaises(ParameterParseException) as cm:
+            ch.StrToRigParameter('pm-non-existent-parameter')
+            
+        # no pm prefix
+        with self.assertRaises(ParameterParseException) as cm:
+            ch.StrToRigParameter('None')
+        # non-existent parameter without prefix
+        with self.assertRaises(ParameterParseException) as cm:
+            ch.StrToRigParameter('non-existent-parameter')
+
+    def test_bytes_and(self):
+        arr1 = bytearray(b'\x10\x22\x80\x55')
+        arr2 = bytearray(b'\x30\xff\x00\xa9')
+        actual_result = ch.BytesAnd(arr1, arr2)
+        expected_result = bytearray(b'\x10\x22\x00\x01')
