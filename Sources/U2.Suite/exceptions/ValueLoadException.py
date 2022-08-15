cef8a8df95bcb8227f1ffaa46263a7916c7623e7
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-08-15 20:01:04 +0300

    Implemented loading of the Status commands

--- Sources/U2.Suite/exceptions/ValueLoadException.py
+++ Sources/U2.Suite/exceptions/ValueLoadException.py
@@ -15,21 +15,8 @@
 # You should have received a copy of the GNU General Public License 
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import unittest
-from os.path import join
-from helpers.FileSystemHelper import FileSystemHelper as fsh
-from helpers.RigHelper import RigHelper as rh
-from contracts.RigCommands import RigCommands
+import sys
 
-class RigHelperTests(unittest.TestCase):
-    def test_LoadIC705(self):
-        folder = fsh.getIniFilesFolder()
-        ini_file_path = join(folder, 'IC-705.ini')
-        rig_commands = rh.loadRigCommands(ini_file_path)
-        self.assertEqual(3, len(rig_commands.InitCmd))
-        self.assertEqual(9, len(rig_commands.StatusCmd))
-        
-    def test_are_equal(self):
-        assert rh.AreEqual('a', 'a')
-        assert not rh.AreEqual('a', 'b')
-        
+class ValueLoadException(Exception):
+    def __init__(self, message):
+        super(ValueLoadException, self).__init__(message)
--- Sources/U2.Suite/helpers/RigHelper.py
+++ Sources/U2.Suite/helpers/RigHelper.py
@@ -20,14 +20,18 @@ import configparser
 from pyparsing import empty
 
 from contracts.BitMask import BitMask
+from contracts.ParameterValue import ParameterValue
 from contracts.RigCommand import RigCommand
 from contracts.RigCommands import RigCommands
 from contracts.RigParameter import RigParameter
 from exceptions.EntryLoadErrorException import EntryLoadErrorException
 from exceptions.IniFileLoadException import IniFileLoadException
 from exceptions.LoadInitCommandsException import LoadInitCommandsException
+from exceptions.LoadStatusCommandsException import LoadStatusCommandsException
 from exceptions.MaskValidationException import MaskValidationException
 from exceptions.UnexpectedEntryException import UnexpectedEntryException
+from exceptions.ValueLoadException import ValueLoadException
+from exceptions.ValueValidationException import ValueValidationException
 from helpers.ConversionHelper import ConversionHelper as ch
 from helpers.IniFileHelper import IniFileHelper as ih
 from helpers.FileSystemHelper import FileSystemHelper as fsh
@@ -35,6 +39,9 @@ from os.path import join
 from typing import List, Tuple
 
 class RigHelper():
+    
+    NumericParameters = [ RigParameter.freq, RigParameter.freqa, 
+                         RigParameter.freqb, RigParameter.pitch, ]
     
     @staticmethod
     def ValidateEntries(entries:List[Tuple[str,str]], allowed_entries:List[str]):
@@ -110,10 +117,15 @@ class RigHelper():
         return result
     
     @staticmethod
+    def LoadSections(config_parser : configparser.ConfigParser, section_name : str) \
+        -> List[RigCommand]:
+        return [f for f in config_parser.sections() if f.lower().find(section_name) > -1]
+    
+    @staticmethod
     def LoadInitCommands(config_parser : configparser.ConfigParser) -> List[RigCommand]:
         result = []
         
-        sections = [f for f in config_parser.sections() if f.lower().find('init') > -1]
+        sections = RigHelper.LoadSections(config_parser, 'init')
         assert len(sections) > 0
         
         for section in sections:
@@ -134,17 +146,193 @@ class RigHelper():
         return result
     
     @staticmethod
-    def loadRigCommands(path) -> RigCommands:
+    def ReadStringFromIni(config_parser: configparser.ConfigParser, section : str,
+        setting_name : str, default_value : str) -> str:
+        ini_setting = config_parser[section][setting_name]
+        if ini_setting != None:
+            return ini_setting
+
+        return default_value
+
+    @staticmethod
+    def ReadIntFromIni(config_parser: configparser.ConfigParser, section : str,
+        setting_name : str, default_value : str) -> int:
+        return int(RigHelper.ReadStringFromIni(config_parser, section, setting_name, '0'))
+    
+    @staticmethod
+    def ParseParameterValue(value : str) -> ParameterValue:
+        if len(value) == 0:
+            return ParameterValue()
+        
+        elements = value.split('|')
+        if len(elements) == 0:
+            return ParameterValue()
+        
+        elif len(elements) != 5 and len(elements) != 6:
+            raise ValueLoadException("A value \'{}\' has invalid syntax.".format(value))
+        
+        result = ParameterValue()
+        
+        if len(elements) == 6:
+            result.Param = ch.StrToRigParameter(elements[5])
+        try:
+            result.Start = int(elements[0])
+        except Exception:
+            raise ValueLoadException(f"invalid Start value in '{value}'")
+        
+        try:
+            result.Len = int(elements[1])
+        except Exception:
+            raise ValueLoadException(f"Invalid Length value in '{value}'")
+        
+        result.Format = ch.StrToValueFormat(elements[2])
+        try:
+            result.Mult = float(elements[3])
+        except Exception as ex:
+            raise ValueLoadException(f"invalid Multiplier value in '{value}'. {ex}")
+        
+        try:
+            result.Add = float(elements[4])
+        except Exception as ex:
+            raise ValueLoadException(f"invalid Add value in '{value}'. {ex}")
+        return result
+
+    @staticmethod
+    def LoadValue(config_parser: configparser.ConfigParser,
+                             section: str, setting_name: str) -> ParameterValue:
+        try:
+            value = RigHelper.ReadStringFromIni(config_parser, section, setting_name, '')
+            return RigHelper.ParseParameterValue(value)
+        
+        except ValueLoadException as ex:
+            raise ex
+        except Exception as ex:
+            raise ValueLoadException(ex.Message)
+        
+    @staticmethod
+    def ValidateValue(value : ParameterValue, size: int):
+        try:
+            if value.Param == RigParameter.none:
+                return
+            if size == 0:
+                size = int.MaxValue
+            if value.Start < 0 or value.Start >= size:
+                raise ValueValidationException("Invalid Start value: {}".format(value.Start))
+            if value.Len < 0 or value.Start + value.Len > size:
+                raise ValueValidationException("invalid Length value. Start={}, Len={}".format(value.Start, value.Len))
+            if value.Mult <= 0:
+                raise ValueValidationException("invalid Multiplier value: {}".format(value.Mult))
+        except Exception as e:
+            raise ValueValidationException(e.Message)
+
+    @staticmethod
+    def ReadStatusEntryValue(cmd: RigCommand, config_parser: configparser.ConfigParser,
+                             section: str, entry: Tuple[str, str]) -> Tuple[bool, ParameterValue]:
+        result = None
+        key = entry[0].lower()
+        try:
+            if not key.startswith('value'):
+                return False, None
+
+            value = RigHelper.LoadValue(config_parser, section, key)
+            RigHelper.ValidateValue(value, max(cmd.ReplyLength, len(cmd.Validation.Mask)))
+
+            if value.Param == RigParameter.none:
+                # parameter name is missing
+                return False, None
+            elif RigHelper.NumericParameters.count(value.Param) == 0:
+                # parameter must be of numeric type
+                return False, None
+
+            return True, value
+        except ValueLoadException:
+            return False, None
+        
+    @staticmethod
+    def ReadStatusEntryFlag(cmd: RigCommand,
+        ini_setting: Tuple[str, str]) -> Tuple[bool, BitMask]:
+    
+        if not ini_setting[0].startswith("flag"):
+            return False, None
+
+        try:
+            flag = ch.StrToBitMask(ini_setting[1])
+            RigHelper.ValidateMask(ini_setting[0], flag, cmd.ReplyLength, cmd.ReplyEnd)
+            return True, flag
+        except MaskValidationException:
+            return False, None
+    
+    @staticmethod
+    def LoadStatusCommandsEntries(config_parser : configparser.ConfigParser,
+                                 section : str, entries : List[Tuple[str, str]],
+                                 cmd : RigCommand):
+        cmd.Values = []
+        cmd.Flags = []
+
+        for entry in entries:
+            status_entry_value = RigHelper.ReadStatusEntryValue(cmd, config_parser, section, entry)
+            if status_entry_value[0]:
+                cmd.Values.append(status_entry_value[1])
+                continue
+            
+            status_entry_flag = RigHelper.ReadStatusEntryFlag(cmd, entry)
+            if status_entry_flag[0]:
+                cmd.Flags.append(status_entry_flag[1])
+
+    @staticmethod
+    def LoadStatusCommands(config_parser : configparser.ConfigParser) -> List[RigCommand]:
+        result = []
+
+        sections = RigHelper.LoadSections(config_parser, 'status')
+        for section in sections:
+            try:
+                entries = ih.LoadSectionSettings(config_parser, section)
+                if (len(entries) == 0):
+                    continue
+                
+                allowed_entries = [
+                    'command', 'replylength', 'replyend', 'validate',
+                    'value', 'value1', 'value2', 'value3', 'value4', 'value5',
+                    'value6', 'value7', 'value8', 'value9', 'value10',
+                    'flag1', 'flag2', 'flag3', 'flag4',
+                    'flag5', 'flag6', 'flag7', 'flag8',
+                    'flag9', 'flag10', 'flag11', 'flag12', 'flag13',
+                    'flag14', 'flag15', 'flag16', 'flag17', 'flag18',
+                    'flag19', 'flag20', 'flag21', 'flag22',
+                ]
+                RigHelper.ValidateEntries(entries, allowed_entries)
+
+                cmd = RigHelper.LoadCommon(config_parser, section)
+
+                if cmd.ReplyLength == 0 and len(cmd.ReplyEnd) == 0:
+                    raise LoadStatusCommandsException("Reply length or reply end must be specified.")
+
+                RigHelper.LoadStatusCommandsEntries(config_parser, section, entries, cmd)
+
+                if len(cmd.Values) == 0 and len(cmd.Flags) == 0:
+                    raise LoadStatusCommandsException("at least one ValueNN or FlagNN must be defined")
+
+                result.append(cmd)
+            except LoadStatusCommandsException as ex:
+                raise ex
+            except Exception as ex:
+                raise LoadStatusCommandsException(f"Error loading of STATUS section. {ex}")
+
+        return result
+    
+    @staticmethod
+    def loadRigCommands(path : str) -> RigCommands:
         config_parser = ih.Create(path)
         
         rig_commands = RigCommands()
         rig_commands.InitCmd = RigHelper.LoadInitCommands(config_parser)
+        rig_commands.StatusCmd = RigHelper.LoadStatusCommands(config_parser)
         
         return rig_commands
     
     @staticmethod
     def loadAllRigCommands(path) -> List[RigCommands]:
-        list = []
+        list = List[RigCommands]()
         iniDirectory = fsh.getIniFilesFolder()
         files = fsh.enumerateDirectory(iniDirectory, '.ini')
         for file in files:
