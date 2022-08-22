a9da9aeb1633333f2a4157f0ca69d5d71f3a60ae
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-08-22 14:29:56 +0300

    Relocated the NewRigDialog

--- Sources/U2.Suite/tests/test_NewRigDialog.py
+++ Sources/U2.Suite/tests/test_NewRigDialog.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License 
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from dialogs.NewRigDialog import NewRigDialog
+from NewRigDialog import NewRigDialog
 import unittest
 
 class test_NewRigDialog(unittest.TestCase):
