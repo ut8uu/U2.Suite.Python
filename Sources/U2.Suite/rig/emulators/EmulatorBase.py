01aec5f624735e813086e684e514ababfec9fa65
Sergey Usmanov <ut8uu@ukr.net>
2022-10-24 22:18:21 +0300

    Removed import of the pty package

--- Sources/U2.Suite/rig/emulators/EmulatorBase.py
+++ Sources/U2.Suite/rig/emulators/EmulatorBase.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License 
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import os, pty, threading
+import os, threading
 from contracts.KnownIdentifiers import KnownIdentifiers
 from contracts.RigCommand import RigCommand
 from contracts.RigCommands import RigCommands
