f9e2dd1830a900936198d530c252d837bd069e88
Sergey Usmanov <ut8uu@ukr.net>
2022-11-27 21:48:28 +0200

    Added all keys and bindings

--- Sources/U2.Suite/Logger_MainWindow.py
+++ Sources/U2.Suite/Logger_MainWindow.py
@@ -17,18 +17,139 @@
 
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import helpers.KeyBinderKeys as kbk
 from logger.ui.Ui_LoggerMainWindow import Ui_LoggerMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from pyqtkeybind import keybinder
+from typing import List
 
 class Logger_MainWindow(QMainWindow, Ui_LoggerMainWindow):
+    _lastSelectedControl: QWidget
+    _allControls : List[QWidget]
+    _registered = False
     
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self._allControls = [
+            self.tbCallsign, self.tbRcv, self.tbSnt, self.tbName, self.tbComment,
+            self.btnF1, self.btnF2, self.btnF3, self.btnF4, self.btnF5, self.btnF6,
+            self.btnF7, self.btnF8, self.btnF9, self.btnF10, self.btnF11, self.btnF12,
+            self.cbBand, self.cbMode, self.cbUtc, self.cbRealTime,
+            self.tdDateTime,
+            ]
+    
+    def getSelectedControl(self) -> QWidget:
+        '''
+        Locates and returns the control that is focused.
+        '''
+        for control in self._allControls:
+            if control.hasFocus():
+                return control
+
+        return None
+
+    def keyPress(self, key_string : str):
+        '''Handles the key by its name'''
+        
+
+    def keyEnterPress(self) -> None:
+        self.keyPress(kbk.KEY_ENTER)
+
+    def keyEscPress(self) -> None:
+        self.keyPress(kbk.KEY_ESC)
+
+    def keyF1Press(self) -> None:
+        self.keyPress(kbk.KEY_F1)
+
+    def keyF2Press(self) -> None:
+        self.keyPress(kbk.KEY_F2)
+
+    def keyF3Press(self) -> None:
+        self.keyPress(kbk.KEY_F3)
+
+    def keyF4Press(self) -> None:
+        self.keyPress(kbk.KEY_F4)
+
+    def keyF5Press(self) -> None:
+        self.keyPress(kbk.KEY_F5)
+
+    def keyF6Press(self) -> None:
+        self.keyPress(kbk.KEY_F6)
+
+    def keyF7Press(self) -> None:
+        self.keyPress(kbk.KEY_F7)
+
+    def keyF8Press(self) -> None:
+        self.keyPress(kbk.KEY_F8)
+
+    def keyF9Press(self) -> None:
+        self.keyPress(kbk.KEY_F9)
+
+    def keyF10Press(self) -> None:
+        self.keyPress(kbk.KEY_F10)
+
+    def keyF11Press(self) -> None:
+        self.keyPress(kbk.KEY_F11)
+
+    def keyF12Press(self) -> None:
+        self.keyPress(kbk.KEY_F12)
+
+    def keySpacePress(self) -> None:
+        self.keyPress(kbk.KEY_SPACE)
+
+    def registerKeys(self) -> None:
+        '''
+        Registers all hotkeys
+        '''
+        if self._registered:
+            return
+
+        keybinder.register_hotkey(self.winId(), kbk.KEY_ENTER, self.keyEnterPress)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_ESC, self.keyEscPress)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F1, self.keyF1Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F2, self.keyF2Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F3, self.keyF3Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F4, self.keyF4Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F5, self.keyF5Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F6, self.keyF6Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F7, self.keyF7Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F8, self.keyF8Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F9, self.keyF9Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F10, self.keyF10Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F11, self.keyF11Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_F12, self.keyF12Press)
+        keybinder.register_hotkey(self.winId(), kbk.KEY_SPACE, self.keySpacePress)
+        self._registered = True
+
+
+    def unregisterKeys(self, window : QMainWindow) -> None:
+        '''
+        Unregisters previously registered keys
+        '''
+        if not self._registered:
+            return
+        self._registered = False
+        keybinder.unregister_hotkey(self, kbk.KEY_ENTER)
+        keybinder.unregister_hotkey(self, kbk.KEY_ESC)
+        keybinder.unregister_hotkey(self, kbk.KEY_F1)
+        keybinder.unregister_hotkey(self, kbk.KEY_F2)
+        keybinder.unregister_hotkey(self, kbk.KEY_F3)
+        keybinder.unregister_hotkey(self, kbk.KEY_F4)
+        keybinder.unregister_hotkey(self, kbk.KEY_F5)
+        keybinder.unregister_hotkey(self, kbk.KEY_F6)
+        keybinder.unregister_hotkey(self, kbk.KEY_F7)
+        keybinder.unregister_hotkey(self, kbk.KEY_F8)
+        keybinder.unregister_hotkey(self, kbk.KEY_F9)
+        keybinder.unregister_hotkey(self, kbk.KEY_F10)
+        keybinder.unregister_hotkey(self, kbk.KEY_F11)
+        keybinder.unregister_hotkey(self, kbk.KEY_F12)
+        keybinder.unregister_hotkey(self, kbk.KEY_SPACE)
 
 if __name__ == '__main__':
     from logger.ui.Ui_LoggerMainWindow import Ui_LoggerMainWindow
     app = QApplication(sys.argv)
     window = Logger_MainWindow()
+
     window.show()
     app.exec()
