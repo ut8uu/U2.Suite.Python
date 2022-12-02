e002ca10344248b54eb85f37b9db257fd8e6351b
Sergey Usmanov <ut8uu@ukr.net>
2022-12-03 00:40:31 +0200

    Added date and time functionality

--- Sources/U2.Suite/Logger_MainWindow.py
+++ Sources/U2.Suite/Logger_MainWindow.py
@@ -15,12 +15,15 @@
 # You should have received a copy of the GNU General Public License 
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from datetime import datetime
 import sys
+from threading import Thread
+import time
 
 import helpers.KeyBinderKeys as kbk
 from logger.logger_constants import *
 from logger.ui.Ui_LoggerMainWindow import Ui_LoggerMainWindow
-from PyQt5.QtCore import QAbstractNativeEventFilter, QAbstractEventDispatcher, pyqtSlot
+from PyQt5.QtCore import QAbstractEventDispatcher, pyqtSlot, QDateTime
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from pyqtkeybind import keybinder
 from typing import List
@@ -33,50 +36,94 @@ class Logger_MainWindow(QMainWindow, Ui_LoggerMainWindow):
     _registered = False
     _win_event_filter : WinEventFilter
     _event_dispatcher : QAbstractEventDispatcher
+
+    _running : bool
+
+    _datetime_utc : bool
+    _datetime_24h : bool
+    _datetime_realtime : bool
+
+    _realtime_thread : Thread
     
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self._running = True
+        
+        # datetime stuff
+        self._datetime_utc = True
+        self._datetime_24h = True
+        self._datetime_realtime = True
+
         self.setupUi(self)
         self._allControls = [
             self.tbCallsign, self.tbRcv, self.tbSnt, self.tbName, self.tbComment,
             self.btnF1, self.btnF2, self.btnF3, self.btnF4, self.btnF5, self.btnF6,
             self.btnF7, self.btnF8, self.btnF9, self.btnF10, self.btnF11, self.btnF12,
-            self.cbBand, self.cbMode, self.cbUtc, self.cbRealTime,
+            self.cbBand, self.cbMode, self.cbUtc, 
+            #self.cbRealTime,
             self.tdDateTime,
             ]
         self.registerKeys()
+        self.tbCallsign.setFocus()
+        self.SetCurrentDateTime()
+
+        #self._realtime_thread = Thread(target = self.realTimeThread)
+        #self._realtime_thread.start()
     
     def __del__(self):
         '''A class' destructor'''
+        self._running = False
         self.unregisterKeys()
-    
+
+    def destroy(self) -> None:
+        self._running = False
+
+    def realTimeThread(self) -> None:
+        '''A code inside the realtime timer'''
+        while self._running:
+            try:
+                if self._datetime_realtime:
+                    focused_control = self.getSelectedControl()
+                    if self._datetime_utc:
+                        self.tdDateTime.setDateTime(QDateTime.currentDateTimeUtc())
+                    else:
+                        self.tdDateTime.setDateTime(QDateTime.currentDateTime())
+                    focused_control.setFocus()
+            except Exception as ex:
+                print(ex.args[0])
+            time.sleep(1)
+
     def getSelectedControl(self) -> QWidget:
-        '''
-        Locates and returns the control that is focused.
-        '''
+        '''Locates and returns the control that is focused.'''
         for control in self._allControls:
             if control.hasFocus():
                 return control
 
-        return None
+        # in case no control found, the Callsign is concidered focused
+        self.tbCallsign.setFocus()
+        return self.tbCallsign
 
     def handleKeySpace(self) -> None:
         '''Handles the SPACE key'''
-        # Callsign
-        if self.tbCallsign.hasFocus():
-            self.tbName.setFocus()
-        # Name
-        elif self.tbName.hasFocus():
-            if len(self.tbName.text()) > 0:
-                self.tbName.setText(self.tbName.text() + ' ')
-            else:
-                self.tbComment.setFocus()
-        # Comment
-        elif self.tbComment.hasFocus():
-            if len(self.tbComment.text()) > 0:
-                self.tbComment.setText(self.tbComment.text() + ' ')
-            else:
-                self.tbCallsign.setFocus()
+        try:
+            # Callsign
+            if self.tbCallsign.hasFocus():
+                self.tbName.setFocus()
+            # Name
+            elif self.tbName.hasFocus():
+                if len(self.tbName.text()) > 0:
+                    self.tbName.setText(self.tbName.text() + ' ')
+                else:
+                    self.tbComment.setFocus()
+            # Comment
+            elif self.tbComment.hasFocus():
+                if len(self.tbComment.text()) > 0:
+                    self.tbComment.setText(self.tbComment.text() + ' ')
+                else:
+                    self.tbCallsign.setFocus()
+        except Exception as ex:
+            print(ex.args[0])
 
     def keyPress(self, key_string : str):
         '''Handles the key by its name'''
@@ -160,14 +207,14 @@ class Logger_MainWindow(QMainWindow, Ui_LoggerMainWindow):
 
         self._registered = True
 
-    def unregisterKeys(self, window : QMainWindow) -> None:
+    def unregisterKeys(self) -> None:
         '''
         Unregisters previously registered keys
         '''
         if not self._registered:
             return
         self._registered = False
-        #keybinder.unregister_hotkey(self, kbk.KEY_RETURN)
+        keybinder.unregister_hotkey(self, kbk.KEY_RETURN)
         keybinder.unregister_hotkey(self, kbk.KEY_SPACE)
         keybinder.unregister_hotkey(self, kbk.KEY_ESC)
         keybinder.unregister_hotkey(self, kbk.KEY_F1)
@@ -182,6 +229,29 @@ class Logger_MainWindow(QMainWindow, Ui_LoggerMainWindow):
         keybinder.unregister_hotkey(self, kbk.KEY_F10)
         keybinder.unregister_hotkey(self, kbk.KEY_F11)
         keybinder.unregister_hotkey(self, kbk.KEY_F12)
+
+    @pyqtSlot()
+    def SetCurrentDateTime(self) -> None:
+        '''Handles the clicking the Now button'''
+        if self._datetime_utc:
+            self.tdDateTime.setDateTime(QDateTime.currentDateTimeUtc())
+        else:
+            self.tdDateTime.setDateTime(QDateTime.currentDateTime())
+
+    @pyqtSlot()
+    def dateTimeCheckBoxChanged(self):
+        '''Handles changing of the date and time related checkboxes'''
+        '''
+        if self.cb24hour.isChecked():
+            self.tdDateTime.setDisplayFormat(DISPLAY_FORMAT_DDMMYY_HHMMSS)
+        else:
+            self.tdDateTime.setDisplayFormat(DISPLAY_FORMAT_MMDDYY_HHMMSS_AP)
+        self._datetime_realtime = self.cbRealTime.isChecked()
+        if self._datetime_realtime and self.tdDateTime.hasFocus():
+            self.tbCallsign.setFocus()
+        self.tdDateTime.setEnabled(not self._datetime_realtime)
+        '''
+        self._datetime_utc = self.cbUtc.isChecked()
 
     @pyqtSlot()
     def bandChanged(self):
@@ -208,16 +278,7 @@ if __name__ == '__main__':
     window = Logger_MainWindow()
 
     window.show()
-    #window.registerKeys()
-    '''keybinder.init()
-    keybinder.register_hotkey(window.winId(), "F1", keyEnterPress)
-    win_event_filter = WinEventFilter(keybinder)
-    event_dispatcher = QAbstractEventDispatcher.instance()
-    event_dispatcher.installNativeEventFilter(win_event_filter)
-'''
 
     app.exec()
-
-#    window.unregisterKeys(window)
-
+    window.destroy()
     del window
